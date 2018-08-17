3d945a8cc56eca84ebfd3dd1bc69baabe9b3ade0
saleeh <saleeh93@gmail.com>
2018-08-18 01:41:41 +0530

    Added test

--- app/src/androidTest/java/xyz/appmaker/keralarescue/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/xyz/appmaker/keralarescue/ExampleInstrumentedTest.java
@@ -4,9 +4,17 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +23,23 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("xyz.appmaker.keralarescue", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> rule  = new  ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void loginErrorMessage() {
+        onView(withId(R.id.loginBtn)).perform(click());
+
+    }
+    @Test
+    public void clickSignUpButton_opensSignUpScreen() {
+        //locate and click on the login button
+
+
+        //check if the sign up screen is displayed by asserting that the first name edittext is displayed
+//        onView(withId(R.id.edit_text_first_name)).check(matches(allOf(isDescendantOfA(withId(R.id.layout_sign_up)), isDisplayed())));
     }
 }
