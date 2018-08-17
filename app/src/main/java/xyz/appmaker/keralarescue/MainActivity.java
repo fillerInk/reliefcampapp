b5d3867feea5d84cc70e8f56bd8f4b5f310fae10
Muneef M <mailmuneef@gmail.com>
2018-08-17 22:28:30 +0530

    - Created Person Registration UI

--- app/src/main/java/xyz/appmaker/keralarescue/MainActivity.java
+++ app/src/main/java/xyz/appmaker/keralarescue/MainActivity.java
@@ -1,5 +1,6 @@
 package xyz.appmaker.keralarescue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import xyz.appmaker.keralarescue.Activities.FieldsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent fieldsAct = new Intent(MainActivity.this, FieldsActivity.class);
+                startActivity(fieldsAct);
             }
         });
     }
