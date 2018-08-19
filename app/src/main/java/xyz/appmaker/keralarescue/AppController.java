85a3900ddc2c582127555acde91435c8370b6a50
saleeh <saleeh93@gmail.com>
2018-08-19 10:27:35 +0530

    Update URL And added interceptor for auto logout

--- app/src/main/java/xyz/appmaker/keralarescue/AppController.java
+++ app/src/main/java/xyz/appmaker/keralarescue/AppController.java
@@ -1,19 +1,30 @@
 package xyz.appmaker.keralarescue;
 
 import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.onesignal.OneSignal;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import xyz.appmaker.keralarescue.Tools.APIService;
 import xyz.appmaker.keralarescue.Tools.Config;
+import xyz.appmaker.keralarescue.Tools.PreferensHandler;
 
 public class AppController extends Application {
     public static APIService RetrofitService;
     static Retrofit retrofit;
+    public static PreferensHandler pref;
+    public static OkHttpClient.Builder httpClient;
 
     @Override
     public void onCreate() {
@@ -22,17 +33,35 @@ public class AppController extends Application {
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
+        pref = new PreferensHandler(this);
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);  // <-- this is the important line!
+
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                Response response = chain.proceed(request);
+                if (response.code() == 401) {
+                    pref.setUserToken("");
+//                    Toast.makeText(AppController.this,"Session expired",Toast.LENGTH_SHORT).show();
+                    Intent actLogin = new Intent(AppController.this, MainActivity.class);
+                    startActivity(actLogin);
+                }
+                return response;
+            }
+        });
+
 
     }
 
     public static APIService getRetrofitInstance() {
-        if (retrofit ==  null) {
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-// set your desired log level
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        if (retrofit == null) {
 
-            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-            httpClient.addInterceptor(logging);  // <-- this is the important line!
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(Config.BASE_URL)
